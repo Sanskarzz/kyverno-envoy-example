a61cd06abdc51a060da6d59817816f5f8731264f
Sanskarzz <sanskar.gur@gmail.com>
2024-02-12 23:09:50 +0530

    minor fix

--- cmd/main.go
+++ cmd/main.go
@@ -32,7 +32,6 @@ func main() {
 			startServer(serveConfig.address, serveConfig.policies)
 		},
 	}
-
 	Cmd.Flags().StringSliceVar(&serveConfig.policies, "policy", nil, "path to policy")
 	Cmd.Flags().StringVar(&serveConfig.address, "address", "localhost:9002", "address to serve on")
 
@@ -59,27 +58,23 @@ func authHandler(policies []string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var checkReq kyvernoauth.CheckRequest
 		var checkRes kyvernoauth.CheckResponse
-
 		// Marshal checkReq into JSON bytes to send in the request body.
 		reqBytes, err := json.Marshal(checkReq)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
-
 		// Unmarshal the JSON request bytes into the payload interface.
 		// This allows the payload to be of any JSON-compatible type.
 		var payload interface{}
 		if err := json.Unmarshal([]byte(reqBytes), &payload); err != nil {
 			panic(err)
 		}
-
 		// Load parses and initializes policy resources.
 		policies, err := policy.Load(policies...)
 		if err != nil {
 			return
 		}
-
 		// enginerequest creates a Request struct containing the resources to check
 		// and the policies to check them against. This request will be passed to
 		// the policy engine for evaluation.
@@ -87,7 +82,6 @@ func authHandler(policies []string) http.HandlerFunc {
 			Resources: []interface{}{payload},
 			Policies:  policies,
 		}
-
 		// Runs the policy engine to evaluate the request against the loaded policies.
 		// Iterates through the results, setting the response status code based on the result status.
 		// 200 if the policy passes, 403 if it fails.
